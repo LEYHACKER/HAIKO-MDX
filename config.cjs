7e7d9f1655f605ca09a35b44e82bb2fb59131f49
LEYHACKER <leyhacker8@gmail.com>
2025-05-10 17:57:05 -0400

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -6,8 +6,8 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "",
-  PREFIX: process.env.PREFIX || '.',
+  SESSION_ID: process.env.SESSION_ID || "HAIKO~MDX~eRBmiTbS#0wfGEYBAqoxiqQk231swlK7Y0sW_1WVc7mxcXlh88jU",
+  PREFIX: process.env.PREFIX || '-',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
   STATUS_READ_MSG: process.env.STATUS_READ_MSG || '',
@@ -28,7 +28,7 @@ const config = {
   MENU_IMAGE: process.env.MENU_IMAGE || "https://files.catbox.moe/voqg33.jpg",
   DESCRIPTION: process.env.DESCRIPTION || "WELCOME TO HAIKO-MDX",
   OWNER_NAME: process.env.OWNER_NAME || "PROFESSEUR",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "529633982655",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "50940140696",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
