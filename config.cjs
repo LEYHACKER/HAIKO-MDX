ba2aa335e90ce659885049531530b42fd01cc691
LEYHACKER <leyhacker8@gmail.com>
2025-05-10 18:07:32 -0400

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const config = {
   SESSION_ID: process.env.SESSION_ID || "HAIKO~MDX~eRBmiTbS#0wfGEYBAqoxiqQk231swlK7Y0sW_1WVc7mxcXlh88jU",
-  PREFIX: process.env.PREFIX || '-',
+  PREFIX: process.env.PREFIX || '+',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
   STATUS_READ_MSG: process.env.STATUS_READ_MSG || '',
